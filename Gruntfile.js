2dc476c26ed6635c8e758c0b87938871553f76d6
Chris Speers <chris.speers@outlook.com>
2016-03-05 10:30:47 -0600

    ...

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,8 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: ['build/','/lib'],
-        typescript: {
-            base: {
-                src: ['lib/**/*.ts'],
-                options: {
-                    module: 'commonjs',
-                    target: 'es5',
-                    declaration: true
-                }
-            }
+        ts: {
+            default:{tsconfig:true}
         },
         karma: {
             unit: {
@@ -52,8 +45,8 @@ module.exports = function (grunt) {
                 // time a new file is added or removed, the Gruntfile has to be updated.
                 files: [
                     { src: 'lib/adal.js', dest: 'build/adal.min.js' },
-                    { src: 'lib/adal-angular.js', dest: 'build/adal-angular.min.js' },
-                ],
+                    { src: 'lib/adal-angular.js', dest: 'build/adal-angular.min.js' }
+                ]
             }
         }
     });
@@ -61,7 +54,7 @@ module.exports = function (grunt) {
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-jsdoc');
     // grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-ts');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-jasmine-node');
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -69,9 +62,9 @@ module.exports = function (grunt) {
     // uglify task is producing invalid js file
 
     // jasmine node directly js api 
-    grunt.registerTask('default', ['typescript','jasmine_node']);
-    grunt.registerTask('angular', ['typescript', 'karma']);
-    grunt.registerTask('doc', ['clean','typescript', 'jsdoc']);
+    grunt.registerTask('default', ['ts','jasmine_node']);
+    grunt.registerTask('angular', ['ts', 'karma']);
+    grunt.registerTask('doc', ['clean','ts', 'jsdoc']);
     grunt.registerTask('minify', ['uglify']);
 
 };
